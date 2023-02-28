79a3a68d36c50bc34d34a6db3e8dddb3982d8c10
Leonawesley <leonajenishlin@gmail.com>
2023-02-28 20:04:45 +0000

    small bootstrap and readme changes

--- src/page-template.js
+++ src/page-template.js
@@ -6,7 +6,7 @@ const generateTeam = team => {
         return `
         <div class= "col p-2" >
         <div class="card employee-card h-100" style="width: 18rem;">
-        <div class="card-header">
+        <div class="card-header bg-primary text-white">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -27,7 +27,7 @@ const generateTeam = team => {
         return `
         <div class= "col p-2" >
         <div class="card employee-card h-100" style="width: 18rem;">
-    <div class="card-header">
+    <div class="card-header bg-primary text-white">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
@@ -48,7 +48,7 @@ const generateTeam = team => {
         return `
         <div class= "col p-2" >
         <div class="card employee-card h-100" style="width: 18rem;">
-    <div class="card-header">
+    <div class="card-header bg-primary text-white">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
@@ -106,8 +106,8 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+            <div class="col-12 jumbotron mb-3 team-heading bg-danger text-white">
+                <h1 class="text-center ">My Team</h1>
             </div>
         </div>
     </div>
